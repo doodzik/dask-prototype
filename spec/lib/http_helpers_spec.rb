516c977af12280bb11329aca277f9d92681870cd
doodzik <frederik.dudzik@gmail.com>
2014-12-16 21:01:07 +0100

    test api helpers

--- spec/lib/http_helpers_spec.rb
+++ spec/lib/http_helpers_spec.rb
@@ -1,7 +1,29 @@
 require 'support_http'
-
 require 'http_helpers'
 
-# describe Helpers do
-#   skip 'no specs yet'
-# end
+describe Api::Helpers do
+  context '.authenticate!' do
+    before do
+      @object = Object.new
+      @object.extend(Api::Helpers)
+      @user = instance_double('User')
+      allow(Mongodb::User).to receive(:current)
+        .with('token').and_return(@user)
+      @auth = allow(@user).to receive(:authenticated?).with('token')
+    end
+
+    it 'succeeds' do
+      @auth.and_return(true)
+      expect(@object.authenticate!('token')).to eq(@user)
+    end
+
+    it 'fails' do
+      @auth.and_return(false)
+      expect(@object.authenticate!('token')).to eq(false)
+    end
+  end
+
+  it 'validator within' do
+    skip 'I have currently no idea how to test this'
+  end
+end
