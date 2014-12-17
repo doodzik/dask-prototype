7a28506ec924cf578696ad67b2641f038ad510c0
doodzik <frederik.dudzik@gmail.com>
2014-12-17 02:54:15 +0100

    add rubocop-rspec fix #19

--- spec/lib/helpers_api_spec.rb
+++ spec/lib/helpers_api_spec.rb
@@ -3,23 +3,22 @@ require 'helpers_api'
 
 describe Api::Helpers do
   context '.authenticate!' do
+    let(:helper) { Object.new.extend(described_class) }
+    let(:user) { instance_double('User') }
+    let(:auth) { allow(user).to receive(:authenticated?).with('token') }
     before do
-      @object = Object.new
-      @object.extend(described_class)
-      @user = instance_double('User')
       allow(Mongodb::User).to receive(:current)
-        .with('token').and_return(@user)
-      @auth = allow(@user).to receive(:authenticated?).with('token')
+        .with('token').and_return(user)
     end
 
     it 'succeeds' do
-      @auth.and_return(true)
-      expect(@object.authenticate!('token')).to eq(@user)
+      auth.and_return(true)
+      expect(helper.authenticate!('token')).to eq(user)
     end
 
     it 'fails' do
-      @auth.and_return(false)
-      expect(@object.authenticate!('token')).to eq(false)
+      auth.and_return(false)
+      expect(helper.authenticate!('token')).to eq(false)
     end
   end
 
--- spec/lib/user_mongoid_spec.rb
+++ spec/lib/user_mongoid_spec.rb
@@ -1,111 +1,114 @@
 require 'support_mongoid'
 require 'user_mongoid'
 
-describe Mongodb::User do
-  it { should have_fields(:email, :token, :password_hash) }
-  it { should embed_many(:tasks) }
-  it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email).case_insensitive }
+describe Mongodb do
+  describe Mongodb::User do
+    it { should have_fields(:email, :token, :password_hash) }
+    it { should embed_many(:tasks) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
 
-  it '.extendet_new' do
-    user = instance_double('User')
-    allow(Mongodb::User).to receive(:new).with(email: 'email').and_return(user)
-    allow(user).to receive(:password=).with('password')
-    allow(Mongodb::User).to receive(:generate_token).and_return('generated')
-    allow(user).to receive(:token=).with('generated')
-    expect(Mongodb::User.extendet_new(email: 'email', pw: 'password'))
-      .to eql(user)
-  end
-
-  context '.current' do
-    it 'fails' do
-      allow(described_class).to receive(:find_by)
-        .with(token: 'token').and_return(false)
-      expect(described_class.current('token'))
-        .to be_instance_of(Mongodb::NullUser)
-    end
-
-    it 'succeeds' do
+    it '.extendet_new' do
       user = instance_double('User')
-      allow(described_class).to receive(:find_by)
-        .with(token: 'token').and_return(user)
-      expect(described_class.current('token')).to eql(user)
-    end
-  end
-
-  it '#password' do
-    user = described_class.new(password_hash: 'hallo',
-                               email: 'x', token: 'token')
-    allow(described_class::Password).to receive(:new)
-      .with('hallo').and_return('hallo')
-    user.password
-    expect(user.instance_variable_get(:@password)).to eql('hallo')
-    allow(described_class::Password).to receive(:new)
-      .with('hallo2').and_return('hallo2')
-    user.password
-    expect(user.instance_variable_get(:@password)).to eql('hallo')
-  end
-
-  it '#password=' do
-    user = described_class.new(email: 'x', token: 'token')
-    allow(described_class::Password).to receive(:create)
-      .with('hallo').and_return('Hallo')
-    user.password = 'hallo'
-    expect(user.password_hash).to eql('Hallo')
-  end
-
-  it '#compare_password' do
-    user = described_class.new(email: 'x', token: 'token')
-    allow(user).to receive(:password).and_return('hallo')
-    expect(user.compare_password('hallo')).to be true
-  end
-
-  it '#authenticated?' do
-    user = described_class.new(email: 'x', token: 'token')
-    allow(Auth).to receive(:secure_compare).with('given_token', 'token')
-    user.authenticated?('given_token')
-  end
-
-  context '.login' do
-    it 'fails' do
-      user = instance_double('User', compare_password: false)
-      allow(described_class).to receive(:find_by)
+      allow(described_class).to receive(:new)
         .with(email: 'email').and_return(user)
-      expect(described_class.login('email', 'pw'))
-        .to be_instance_of(Mongodb::NullUser)
+      allow(user).to receive(:password=).with('password')
+      allow(described_class).to receive(:generate_token).and_return('generated')
+      allow(user).to receive(:token=).with('generated')
+      expect(described_class.extendet_new(email: 'email', pw: 'password'))
+        .to eql(user)
     end
 
-    it 'succeeds' do
-      user = instance_double('User', compare_password: true)
-      allow(user).to receive(:token=).with('token')
-      allow(described_class).to receive(:find_by)
-        .with(email: 'email').and_return(user)
-      allow(described_class).to receive(:generate_token).and_return('token')
-      expect(described_class.login('email', 'pw')).to eql(user)
+    context '.current' do
+      it 'fails' do
+        allow(described_class).to receive(:find_by)
+          .with(token: 'token').and_return(false)
+        expect(described_class.current('token'))
+          .to be_instance_of(Mongodb::NullUser)
+      end
+
+      it 'succeeds' do
+        user = instance_double('User')
+        allow(described_class).to receive(:find_by)
+          .with(token: 'token').and_return(user)
+        expect(described_class.current('token')).to eql(user)
+      end
+    end
+
+    it '#password' do
+      user = described_class.new(password_hash: 'hallo',
+                                 email: 'x', token: 'token')
+      allow(described_class::Password).to receive(:new)
+        .with('hallo').and_return('hallo')
+      user.password
+      expect(user.instance_variable_get(:@password)).to eql('hallo')
+      allow(described_class::Password).to receive(:new)
+        .with('hallo2').and_return('hallo2')
+      user.password
+      expect(user.instance_variable_get(:@password)).to eql('hallo')
+    end
+
+    it '#password=' do
+      user = described_class.new(email: 'x', token: 'token')
+      allow(described_class::Password).to receive(:create)
+        .with('hallo').and_return('Hallo')
+      user.password = 'hallo'
+      expect(user.password_hash).to eql('Hallo')
+    end
+
+    it '#compare_password' do
+      user = described_class.new(email: 'x', token: 'token')
+      allow(user).to receive(:password).and_return('hallo')
+      expect(user.compare_password('hallo')).to be true
+    end
+
+    it '#authenticated?' do
+      user = described_class.new(email: 'x', token: 'token')
+      allow(Auth).to receive(:secure_compare).with('given_token', 'token')
+      user.authenticated?('given_token')
+    end
+
+    context '.login' do
+      it 'fails' do
+        user = instance_double('User', compare_password: false)
+        allow(described_class).to receive(:find_by)
+          .with(email: 'email').and_return(user)
+        expect(described_class.login('email', 'pw'))
+          .to be_instance_of(Mongodb::NullUser)
+      end
+
+      it 'succeeds' do
+        user = instance_double('User', compare_password: true)
+        allow(user).to receive(:token=).with('token')
+        allow(described_class).to receive(:find_by)
+          .with(email: 'email').and_return(user)
+        allow(described_class).to receive(:generate_token).and_return('token')
+        expect(described_class.login('email', 'pw')).to eql(user)
+      end
+    end
+
+    it '.generate_token' do
+      allow(Auth).to receive(:generate_unique_token)
+        .and_yield('value').and_return('valueX')
+      allow(described_class).to receive(:find_by).with(token: 'value')
+      expect(described_class.generate_token).to eql('valueX')
+    end
+
+    it '#to_bearer' do
+      user = described_class.new(email: 'x', token: 'token')
+      expect(user.to_bearer).to eql(access_token: 'token', token_type: 'bearer')
+    end
+
+    it '#to_json' do
+      user = described_class.new(email: 'x')
+      expect(user.to_json).to eql("{\"user\":#{user.as_json}}")
     end
   end
 
-  it '.generate_token' do
-    allow(Auth).to receive(:generate_unique_token)
-      .and_yield('value').and_return('valueX')
-    allow(described_class).to receive(:find_by).with(token: 'value')
-    expect(described_class.generate_token).to eql('valueX')
-  end
-
-  it '#to_bearer' do
-    user = described_class.new(email: 'x', token: 'token')
-    expect(user.to_bearer).to eql(access_token: 'token', token_type: 'bearer')
-  end
-
-  it '#to_json' do
-    user = described_class.new(email: 'x')
-    expect(user.to_json).to eql("{\"user\":#{user.as_json}}")
-  end
-end
-
-describe Mongodb::NullUser do
-  it 'returns false' do
-    null_user = described_class.new
-    expect(null_user.missing_method).to be false
+  describe Mongodb::NullUser do
+    it 'returns false' do
+      null_user = described_class.new
+      expect(null_user.missing_method).to be false
+    end
   end
 end
