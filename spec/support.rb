d9a2d48d4332fd0c3480fe740f10c892fe076ff3
doodzik <frederik.dudzik@gmail.com>
2014-12-20 09:55:14 +0100

    try fixing travis

--- spec/support.rb
+++ spec/support.rb
@@ -1,6 +1,3 @@
-$LOAD_PATH.unshift(File.join("#{File.dirname(__FILE__)}/..", 'lib'))
-
-require 'rspec'
 require 'simplecov'
 
 SimpleCov.start do
@@ -10,6 +7,10 @@ end
 
 SimpleCov.minimum_coverage 99
 SimpleCov.refuse_coverage_drop
+
+$LOAD_PATH.unshift(File.join("#{File.dirname(__FILE__)}/..", 'lib'))
+
+require 'rspec'
 
 RSpec.configure do |config|
   # Use color in STDOUT
